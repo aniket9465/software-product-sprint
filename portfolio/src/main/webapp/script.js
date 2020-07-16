3b764528d63ccaeab51567f478dc413ec127965f
Aniket Goyal <aniket14goyal@gmail.com>
2020-07-17 03:13:24 +0530

    Add commenting feature to portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,3 +11,30 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+comments = [];
+
+updateComments = ()=>{
+    var commentsDiv = document.querySelector('#comments');
+    commentsDiv.innerHTML =comments.map(function (commentText) {
+        return '<div class=\"col-lg-12 col-md-12 col-sm-12 col-xs-12\"><h2 class=\"aboutme\">'+commentText+'</h2></div></div><div class="col-xs-8 line" ><hr></div>';
+    }).join('');
+}
+
+fetch('/comment')
+  .then(response => response.json())
+  .then(data =>{ comments = data.map((obj)=>obj.commentText); updateComments()});
+
+$(function() {
+    $('#commentForm').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: 'comment',
+            data:$('#commentForm').serialize(),
+        });
+        comments = [$('#commentBox')[0].value,...comments];
+        updateComments();
+        $('#commentForm')[0].reset();
+        return false;
+    }); 
+})
